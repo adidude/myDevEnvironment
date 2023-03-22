9476df220b907421928ed09a97e66f2ae1ae54ff
adikavemen <menon.or.adithya@gmail.com>
2023-03-22 01:34:18 +0000

    now using ideal permission for each command

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # TODO: Figure out how to only use 80% max battery
 # TODO: Before running enable flatpak & AURs
-pacman -Syu --noconfirm
-pacman -S --needed --noconfirm base-devel
-sed -i "/MAKEFLAGS=\"-j2\"/c\MAKEFLAGS=\"-j$(nproc)\"" /etc/makepkg.conf
-pacman -S --noconfirm ncspot thunderbird code docker
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel
+sudo sed -i "/MAKEFLAGS=\"-j2\"/c\MAKEFLAGS=\"-j$(nproc)\"" /etc/makepkg.conf
+sudo pacman -S --noconfirm ncspot thunderbird code docker
 mkdir home/$USER/Utility
 mkdir home/$USER/Logs
 cd /home/$USER/Utility
 git clone https://aur.archlinux.org/yay.git
 cd ./yay
 makepkg -si --noconfirm
-yay -S --noconfirm insomnia-bin nordvpn-bin
+sudo yay -S --noconfirm insomnia-bin nordvpn-bin
 gpasswd -a $USER nordvpn
-flatpak install -y flathub com.github.tchx84.Flatseal com.discordapp.Discord
-systemctl start docker.service
-systemctl enable docker.service
-systemctl enable --now nordvpnd
-# reboot
+sudo flatpak install -y flathub com.github.tchx84.Flatseal com.discordapp.Discord
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo systemctl enable --now nordvpnd
+# sudo  reboot
